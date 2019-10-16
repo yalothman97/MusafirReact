ba876de20724fd184148b2513046c664eaff3ca6
Yousef Al Othman <Yousef@Yousef.local>
2019-10-16 10:44:28 +0300

    packages page and navbar

--- src/Components/NewNavbar.js
+++ src/Components/NewNavbar.js
@@ -36,54 +36,41 @@ class NewNavbar extends React.Component {
           <MDBNavbar color="black" fixed="top" dark expand="md">
             <MDBContainer>
               <MDBNavbarBrand href="/">
-                <strong>Navbar</strong>
+                <strong className="navstyle">Musafir</strong>
               </MDBNavbarBrand>
               <MDBNavbarToggler onClick={this.onClick} />
               <MDBCollapse isOpen={this.state.collapse} navbar>
-                <MDBNavbarNav left>
+                <MDBNavbarNav right>
                   <MDBNavItem active>
-                    <MDBNavLink to="#">Home</MDBNavLink>
+                    <MDBNavLink to="/">
+                      <p className="navstyle">Home</p>
+                    </MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
-                    <MDBNavLink to="#">Link</MDBNavLink>
+                    <MDBNavLink to="/packages">
+                      <p className="navstyle">Packages</p>
+                    </MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
-                    <MDBNavLink to="#">Profile</MDBNavLink>
+                    <MDBNavLink to="/login">
+                      <p className="navstyle">Login</p>
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">
+                      <p className="navstyle">Sign Up</p>
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">
+                      <p className="navstyle">Cart</p>
+                    </MDBNavLink>
                   </MDBNavItem>
                 </MDBNavbarNav>
               </MDBCollapse>
             </MDBContainer>
           </MDBNavbar>
-
-          <MDBView src="https://mdbootstrap.com/img/Photos/Others/img%20(51).jpg">
-            <MDBMask
-              overlay="black-strong"
-              className="flex-center flex-column text-white text-center"
-            >
-              <h2>This Navbar isn't fixed</h2>
-              <h5>When you scroll down it will disappear</h5>
-              <br />
-              <p>
-                Full page intro with background image will be always displayed
-                in full screen mode, regardless of device{" "}
-              </p>
-            </MDBMask>
-          </MDBView>
         </header>
-
-        <main>
-          <MDBContainer className="text-center my-5">
-            <p align="justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </MDBContainer>
-        </main>
       </div>
     );
   }
